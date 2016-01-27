b3e464e4cff5716eaac212a754561aa3a7e7a9a3
Marc Risney <mrisney@steelhouse.com>
2016-01-27 11:37:56 -0800

    Changes to be committed: 	modified:   app.js 	modified:   index.html 	modified:   scripts/typings/curvecontrol.js 	modified:   scripts/typings/polygonsubdivision.js 	new file:   scripts/typings/signals/Signal.js 	new file:   tsconfig.json

--- src/scripts/typings/curvecontrol.js
+++ src/scripts/typings/curvecontrol.js
@@ -1,21 +1,5 @@
-﻿/// <reference path="./kinetic/kinetic.d.ts" />
-/// <reference path="./polygonsubdivision.ts" />
 var Curves;
 (function (Curves) {
-    Curves.controlPointLayer;
-    Curves.controlLineLayer;
-    Curves.simplifiedPoints;
-    Curves.kineticControlPoints;
-    Curves.stage;
-    Curves.lineTolerance;
-    Curves.drawPoints;
-
-    Curves.subdivisionPoint;
-    Curves.chaikinCurve;
-    Curves.polylineSimplify;
-
-    Curves.curveControl;
-
     var CurveControl = (function () {
         function CurveControl(containerName, w, h) {
             Curves.stage = new Kinetic.Stage({ container: containerName, width: w, height: h });
@@ -23,41 +7,22 @@ var Curves;
             Curves.controlLineLayer = new Kinetic.Layer();
             Curves.simplifiedPoints = new Array();
             Curves.kineticControlPoints = new Array();
-
             Curves.chaikinCurve = new PolygonSubdivision.ChaikinCurve();
+            Curves.bezierCurve = new PolygonSubdivision.BezierCurve();
             Curves.polylineSimplify = new PolygonSubdivision.PolylineSimplify();
             Curves.drawPoints = new Array();
             this.curvePoints = new Array();
+            this.controlPoints = new Array();
             Curves.lineTolerance = 1.0;
             Curves.curveControl = this;
         }
-        CurveControl.prototype.addControlPoint = function (x, y) {
-            var controlPoint = new Kinetic.Circle({
-                x: x,
-                y: y,
-                radius: 10,
-                stroke: '#666',
-                fill: '#ddd',
-                strokeWidth: 2,
-                text: 'p1',
-                draggable: true
-            });
-            Curves.controlPointLayer.add(controlPoint);
-
-            var point1 = new PolygonSubdivision.Point(x, y);
-            var point2 = new PolygonSubdivision.Point(x + 1, y + 1);
-            var point3 = new PolygonSubdivision.Point(x + 2, y + 2);
-            var points = new Array();
-            points.push(point1);
-            points.push(point2);
-            points.push(point3);
-
-            var curvePoints = Curves.chaikinCurve.subdivide(points, 7);
-        };
         CurveControl.prototype.getCurvePoints = function () {
             return this.curvePoints;
         };
-
+        CurveControl.prototype.renderControlPoints = function () {
+            console.log('hiding control points');
+            Curves.stage.clear();
+        };
         CurveControl.prototype.createControlPoint = function (x, y) {
             var controlPoint = new Kinetic.Circle({
                 x: x,
@@ -69,28 +34,21 @@ var Curves;
                 text: 'p1',
                 draggable: true
             });
-
             return controlPoint;
         };
-
-        CurveControl.prototype.setLineTolerance = function (event) {
-            var toleranceSlider = document.getElementById("line-tolerance-range");
-            Curves.lineTolerance = +toleranceSlider.value;
-            Curves.simplifiedPoints = Curves.polylineSimplify.simplify(Curves.drawPoints, Curves.lineTolerance, true);
+        CurveControl.prototype.setLineTolerance = function (lineTolerance) {
+            Curves.simplifiedPoints = Curves.polylineSimplify.simplify(Curves.drawPoints, lineTolerance, true);
             this.curvePoints = Curves.simplifiedPoints;
             Curves.stage.clear();
             Curves.controlLineLayer.removeChildren();
             Curves.controlPointLayer.removeChildren();
             var kineticLinePoints = [];
             Curves.kineticControlPoints.length = 0;
-
             for (var i = 0; i < Curves.simplifiedPoints.length; i++) {
                 var x = Curves.simplifiedPoints[i].x;
                 var y = Curves.simplifiedPoints[i].y;
-
                 kineticLinePoints.push(x);
                 kineticLinePoints.push(y);
-
                 var kineticControlPoint = new Kinetic.Circle({
                     x: x,
                     y: y,
@@ -100,13 +58,12 @@ var Curves;
                     strokeWidth: 2,
                     draggable: true
                 });
-                kineticControlPoint.on('dragstart dragmove', function () {
+                kineticControlPoint.on('dragmove  touchmove', function () {
                     Curves.curveControl.updateControlLines();
                 });
                 Curves.kineticControlPoints.push(kineticControlPoint);
                 Curves.controlPointLayer.add(kineticControlPoint);
             }
-
             var redLine = new Kinetic.Line({
                 points: kineticLinePoints,
                 stroke: "red",
@@ -117,7 +74,6 @@ var Curves;
             Curves.controlLineLayer.add(redLine);
             var plotpoints = [];
             var curvePoints = Curves.chaikinCurve.subdivide(Curves.simplifiedPoints, 7);
-
             for (var j in curvePoints) {
                 plotpoints.push(curvePoints[j].x);
                 plotpoints.push(curvePoints[j].y);
@@ -129,30 +85,28 @@ var Curves;
                 lineCap: 'round',
                 lineJoin: 'round'
             });
-
             Curves.controlLineLayer.add(blueLine);
             Curves.controlLineLayer.drawScene();
             Curves.controlPointLayer.drawScene();
             Curves.stage.add(Curves.controlPointLayer);
             Curves.stage.add(Curves.controlLineLayer);
+            var customEvent = document.createEvent('CustomEvent');
+            customEvent.initCustomEvent('pointEditListener', true, true, curvePoints);
+            document.dispatchEvent(customEvent);
         };
-
         CurveControl.prototype.updateControlLines = function () {
             Curves.controlLineLayer.removeChildren();
             Curves.controlPointLayer.drawScene();
             var simplifiedPoints = new Array();
-
             var kineticLinePoints = [];
             for (var i in Curves.kineticControlPoints) {
                 var x = Curves.kineticControlPoints[i].getAbsolutePosition().x;
                 var y = Curves.kineticControlPoints[i].getAbsolutePosition().y;
-
                 var point = new PolygonSubdivision.Point(x, y);
                 simplifiedPoints.push(point);
                 kineticLinePoints.push(x);
                 kineticLinePoints.push(y);
             }
-
             var redLine = new Kinetic.Line({
                 points: kineticLinePoints,
                 stroke: 'red',
@@ -165,7 +119,6 @@ var Curves;
             var curvePoints = Curves.chaikinCurve.subdivide(simplifiedPoints, 7);
             console.log('number of curve Points = ' + JSON.stringify(curvePoints.length));
             this.curvePoints = curvePoints;
-
             for (var j in curvePoints) {
                 plotpoints.push(curvePoints[j].x);
                 plotpoints.push(curvePoints[j].y);
@@ -177,61 +130,19 @@ var Curves;
                 lineCap: 'round',
                 lineJoin: 'round'
             });
-
             Curves.controlLineLayer.add(blueLine);
             Curves.controlLineLayer.drawScene();
+            var event = document.createEvent('CustomEvent');
+            event.initCustomEvent('pointEditListener', true, true, curvePoints);
+            document.dispatchEvent(event);
         };
-
-        CurveControl.prototype.drawBesizerCurve = function (event) {
-            /*
-            controlPoints.startPoint.on('touchstart mousedown', function () {
-            controlPoints.main.createBall();
-            });
-            
-            
-            
-            controlPoints.startPoint.on('dragstart dragmove', function () {
-            
-            controlPoints.main.removeSurfaces();
-            controlPoints.main.createCurvedSurface();
-            });
-            
-            controlPoints.point1.on('dragstart dragmove', function () {
-            controlPoints.main.removeSurfaces();
-            controlPoints.main.createCurvedSurface();
-            });
-            
-            controlPoints.point2.on('dragstart dragmove', function () {
-            controlPoints.main.removeSurfaces();
-            controlPoints.main.createCurvedSurface();
-            });
-            
-            controlPoints.endPoint.on('dragstart dragmove', function () {
-            controlPoints.main.removeSurfaces();
-            controlPoints.main.createCurvedSurface();
-            });
-            
-            
-            layer.add(controlPoints.startPoint);
-            
-            // layer.add(controlPoints.point1);
-            layer.add(controlPoints.point2);
-            layer.add(controlPoints.endPoint);
-            
-            
-            
-            //layer.add(circle);
-            stage.add(layer);
-            // kineticCurve.createAnchor(10, 10);
-            // kineticCurve.drawCurves();
-            /// var jsonString = JSON.stringify(kineticCurve);
-            // console.log(jsonString);
-            */
+        CurveControl.prototype.drawBesizerCurve = function (controlPoints) {
+            this.controlPoints = controlPoints;
+            var curvePoints = Curves.bezierCurve.subdivide(controlPoints, 1);
+            return curvePoints;
         };
-
-        CurveControl.prototype.drawLine = function (event) {
+        CurveControl.prototype.drawLine = function () {
             Curves.lineTolerance = 0;
-
             Curves.stage.clear();
             Curves.drawPoints.length = 0;
             var layer = new Kinetic.Layer();
@@ -240,31 +151,20 @@ var Curves;
                 y: 0,
                 width: Curves.stage.getWidth(),
                 height: Curves.stage.getHeight(),
-                fill: "white"
+                fill: "transparent"
             });
             layer.add(background);
             layer.draw();
             Curves.stage.add(layer);
             layer.draw();
-
-            // a flag we use to see if we're dragging the mouse
             var isMouseDown = false;
-
-            // a reference to the line we are currently drawing
             var newline;
-
-            // a reference to the array of points making newline
             var points = new Array();
             var tempPoints = [];
-
-            // on the background
-            // listen for mousedown, mouseup and mousemove events
-            background.on('mousedown', function () {
+            background.on('mousedown touchstart', function () {
                 isMouseDown = true;
-                console.log('mouse down');
+                console.log('mouse down from curve control');
                 tempPoints = [];
-
-                //drawPoints.push(new PolygonSubdivision.Point(stage.getPointerPosition().x, stage.getPointerPosition().y));
                 points.push(new PolygonSubdivision.Point(Curves.stage.getPointerPosition().x, Curves.stage.getPointerPosition().y));
                 tempPoints = [];
                 tempPoints.push(Curves.stage.getPointerPosition().x);
@@ -277,35 +177,32 @@ var Curves;
                     lineJoin: 'round'
                 });
                 layer.add(line);
-
                 newline = line;
             });
-            background.on('mouseup', function () {
+            background.on('mouseup touchend', function () {
                 isMouseDown = false;
-
+                console.log('mouse up from curve control');
+                this.curvePoints = Curves.polylineSimplify.simplify(points, Curves.lineTolerance, true);
                 var newpoints = Curves.polylineSimplify.simplify(points, Curves.lineTolerance, true);
+                var event = document.createEvent('CustomEvent');
+                event.initCustomEvent('pointEditListener', true, true, newpoints);
+                document.dispatchEvent(event);
             });
-            background.on('mousemove', function () {
+            background.on('mousemove touchmove', function () {
                 if (!isMouseDown) {
                     return;
                 }
                 ;
-
-                //console.log('mouse moving' + stage.getPointerPosition().x);
                 points.push(new PolygonSubdivision.Point(Curves.stage.getPointerPosition().x, Curves.stage.getPointerPosition().y));
                 Curves.drawPoints.push(new PolygonSubdivision.Point(Curves.stage.getPointerPosition().x, Curves.stage.getPointerPosition().y));
                 tempPoints.push(Curves.stage.getPointerPosition().x);
                 tempPoints.push(Curves.stage.getPointerPosition().y);
-
-                //newline.setPoints(points);
-                // use layer.drawScene
-                // this is faster since the "hit" canvas is not refreshed
                 layer.drawScene();
             });
             Curves.stage.add(layer);
+            return points;
         };
         return CurveControl;
-    })();
+    }());
     Curves.CurveControl = CurveControl;
 })(Curves || (Curves = {}));
-//# sourceMappingURL=curvecontrol.js.map
--- src/scripts/typings/polygonsubdivision.js
+++ src/scripts/typings/polygonsubdivision.js
@@ -1,4 +1,4 @@
-﻿var PolygonSubdivision;
+var PolygonSubdivision;
 (function (PolygonSubdivision) {
     var Point = (function () {
         function Point(x, y) {
@@ -6,7 +6,7 @@
             this.y = y;
         }
         return Point;
-    })();
+    }());
     PolygonSubdivision.Point = Point;
     var WeightedPoint = (function () {
         function WeightedPoint(point, weight) {
@@ -14,7 +14,7 @@
             this.weight = weight;
         }
         return WeightedPoint;
-    })();
+    }());
     PolygonSubdivision.WeightedPoint = WeightedPoint;
     var ChaikinCurve = (function () {
         function ChaikinCurve() {
@@ -22,30 +22,21 @@
         ChaikinCurve.prototype.subdivide = function (points, epsilon) {
             do {
                 var numOfPoints = points.length;
-
-                // keep the first point
                 points.push(points[0]);
-
                 for (var i = 1; i < (numOfPoints - 1); i += 1) {
                     var j = i - 1;
-
-                    // first point
                     var p1 = points[j];
                     var p2 = points[j + 1];
                     var weightedPoints = new Array();
                     weightedPoints.push(new WeightedPoint(p1, 1));
                     weightedPoints.push(new WeightedPoint(p2, 1));
                     points.push(this.affineCombination(weightedPoints));
-
-                    // second point
                     var p3 = points[j + 2];
                     weightedPoints.push(new WeightedPoint(p1, 1));
                     weightedPoints.push(new WeightedPoint(p2, 6));
                     weightedPoints.push(new WeightedPoint(p3, 1));
                     points.push(this.affineCombination(weightedPoints));
                 }
-
-                // third point
                 var k = numOfPoints - 2;
                 var p4 = points[k];
                 var p5 = points[k + 1];
@@ -53,52 +44,41 @@
                 weightedPoints.push(new WeightedPoint(p4, 1));
                 weightedPoints.push(new WeightedPoint(p5, 1));
                 points.push(this.affineCombination(weightedPoints));
-
-                // finally, push the last point
                 points.push(points[numOfPoints - 1]);
-
                 for (var l = 0; l < numOfPoints; ++l)
                     points.shift();
-            } while(--epsilon > 0);
+            } while (--epsilon > 0);
             return points;
         };
-
         ChaikinCurve.prototype.affineCombination = function (weightedPoints) {
             var result = new Point(0, 0);
             var sum = 0;
-
             for (var i = 0; i < weightedPoints.length; i++) {
                 sum += weightedPoints[i].weight;
             }
-
             for (var j = 0; j < weightedPoints.length; j++) {
                 result = this.plus(result, this.scale(weightedPoints[j].point, weightedPoints[j].weight / sum));
             }
             return result;
         };
-
         ChaikinCurve.prototype.plus = function (point1, point2) {
             return new Point(point1.x + point2.x, point1.y + point2.y);
         };
-
         ChaikinCurve.prototype.scale = function (point, scale) {
             return new Point(point.x * scale, point.y * scale);
         };
         return ChaikinCurve;
-    })();
+    }());
     PolygonSubdivision.ChaikinCurve = ChaikinCurve;
-
     var BezierCurve = (function () {
         function BezierCurve() {
         }
         BezierCurve.prototype.subdivide = function (controlPoints, epsilon) {
-            var results;
-            epsilon = epsilon / 100;
+            var results = new Array();
             var startPoint = controlPoints[0];
             var point1 = controlPoints[1];
             var point2 = controlPoints[2];
             var endPoint = controlPoints[3];
-
             for (var i = 0; i < epsilon; i += 0.01) {
                 var ptOnCurve = this.getCubicBezierPointatIndex(startPoint, point1, point2, endPoint, i);
                 results.push(ptOnCurve);
@@ -110,71 +90,57 @@
             var y = this.CubicN(T, startPt.y, controlPt1.y, controlPt2.y, endPt.y);
             return new Point(x, y);
         };
-
         BezierCurve.prototype.CubicN = function (T, a, b, c, d) {
             var t2 = T * T;
             var t3 = t2 * T;
-            return a + (-a * 3 + T * (3 * a - a * T)) * T + (3 * b + T * (-6 * b + b * 3 * T)) * T + (c * 3 - c * 3 * T) * t2 + d * t3;
+            return a + (-a * 3 + T * (3 * a - a * T)) * T
+                + (3 * b + T * (-6 * b + b * 3 * T)) * T
+                + (c * 3 - c * 3 * T) * t2
+                + d * t3;
         };
         return BezierCurve;
-    })();
+    }());
     PolygonSubdivision.BezierCurve = BezierCurve;
-
     var PolylineSimplify = (function () {
         function PolylineSimplify() {
         }
-        // square distance between 2 points
         PolylineSimplify.prototype.getSqDist = function (p1, p2) {
             var dx = p1.x - p2.x, dy = p1.y - p2.y;
             return dx * dx + dy * dy;
         };
-
-        // square distance from a point to a segment
         PolylineSimplify.prototype.getSqSegDist = function (p, p1, p2) {
             var x = p1.x, y = p1.y, dx = p2.x - x, dy = p2.y - y;
-
             if (dx !== 0 || dy !== 0) {
                 var t = ((p.x - x) * dx + (p.y - y) * dy) / (dx * dx + dy * dy);
                 if (t > 1) {
                     x = p2.x;
                     y = p2.y;
-                } else if (t > 0) {
+                }
+                else if (t > 0) {
                     x += dx * t;
                     y += dy * t;
                 }
             }
-
             dx = p.x - x;
             dy = p.y - y;
-
             return dx * dx + dy * dy;
         };
-
-        // basic distance-based simplification
         PolylineSimplify.prototype.simplifyRadialDist = function (points, epsilon) {
             var prevPoint = points[0], newPoints = [prevPoint], point;
-
             for (var i = 1, len = points.length; i < len; i++) {
                 point = points[i];
-
                 if (this.getSqDist(point, prevPoint) > epsilon) {
                     newPoints.push(point);
                     prevPoint = point;
                 }
             }
-
             if (prevPoint !== point)
                 newPoints.push(point);
-
             return newPoints;
         };
-
-        // classic simplification Douglas-Peucker algorithm with recursion elimination
         PolylineSimplify.prototype.simplifyDouglasPeucker = function (points, epsilon) {
             var len = points.length, MarkerArray, markers = new Uint8Array(len), first = 0, last = len - 1, stack = [], newPoints = [], i, maxSqDist, sqDist, index;
-
             markers[first] = markers[last] = 1;
-
             while (last) {
                 maxSqDist = 0;
                 for (i = first + 1; i < last; i++) {
@@ -184,24 +150,19 @@
                         maxSqDist = sqDist;
                     }
                 }
-
                 if (maxSqDist > epsilon) {
                     markers[index] = 1;
                     stack.push(first, index, index, last);
                 }
-
                 last = stack.pop();
                 first = stack.pop();
             }
-
             for (i = 0; i < len; i++) {
                 if (markers[i])
                     newPoints.push(points[i]);
             }
             return newPoints;
         };
-
-        // both algorithms combined for awesome performance
         PolylineSimplify.prototype.simplify = function (points, tolerance, highestQuality) {
             if (points.length <= 1)
                 return points;
@@ -211,7 +172,6 @@
             return points;
         };
         return PolylineSimplify;
-    })();
+    }());
     PolygonSubdivision.PolylineSimplify = PolylineSimplify;
 })(PolygonSubdivision || (PolygonSubdivision = {}));
-//# sourceMappingURL=polygonsubdivision.js.map
